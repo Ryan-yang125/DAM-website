eca2c304694876ecbfca70f878f8585fb40f3dc8
Ryan-yang125 <1598094295@qq.com>
2020-11-01 21:29:51 +0800

    clear

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./components/App";
+import App from "./views/App";
 render(
   <BrowserRouter>
     <App />
--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,12 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-export default function HomePage() {
-  return (
-    <div>
-      <h1>A HomePage</h1>
-      <p>
-        <Link to="/123">Your Link</Link>
-      </p>
-    </div>
-  );
+export default function Homepage() {
+  return <div>homepage</div>;
 }
