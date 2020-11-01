8e001ec8d0caa105923aaa042bf7756061dc0778
Ryan-yang125 <1598094295@qq.com>
2020-11-01 16:11:46 +0800

    server

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const config = require("./config/dev");
+
+const mongoose = require("mongoose");
+const connect = mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.use(cors());
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+app.use(cookieParser());
